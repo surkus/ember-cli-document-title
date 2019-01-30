87c31661a2f1cce35f01bd17ac9cc3acecd310ff
Larry Sprock <larry@lucidbleu.com>
2019-01-30 09:30:33 -0800

    Update index.js to support new name

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
   name: require('./package').name,
 
-  included: function(app) {
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
+  included() {
+    this._super.included.apply(this, arguments);
+    this._ensureFindHost();
 
-    app.import('vendor/document-title/document-title.js');
+    let vendorPath = `vendor/${this.name}`;
+    let host = this._findHost();
+
+    host.import(path.join(vendorPath, 'document-title.js'));
+  },
+
+  _ensureFindHost() {
+    if (!this._findHost) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+
+        return app;
+      };
+    }
   }
 };
